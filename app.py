7f2fdb661a7c0cc543d460b387790fca302755a9
smp200603-a11y <smp200603@gmail.com>
2026-04-23 13:22:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ values = 0.0
 act1="OFF"
 
 def on_publish(client,userdata,result):             #create function for callback
-    print("el dato ha sido publicado \n")
+    print("El dato ha sido publicado \n")
     pass
 
 def on_message(client, userdata, message):
